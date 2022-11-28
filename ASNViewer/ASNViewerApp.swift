b9bbac92a5043f37342223e9e184602d73ef3323
nanashiki <nanashiki.app@gmail.com>
2022-11-29 01:44:13 +0900

    Initial Commit

---
+++ ASNViewer/ASNViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASNViewerApp.swift
+//  ASNViewer
+//
+//  Created by nanashiki on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct ASNViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
